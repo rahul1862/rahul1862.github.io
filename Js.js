49f6855038af4c04ca4649989ed0476f921b555c
rahul1862 <93251748+rahul1862@users.noreply.github.com>
2024-04-05 23:57:20 +0100

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.1.2/firebase-analytics.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyAJHltJPzB2qftaWzdoupVn5NMWuzAWhBQ",
+  authDomain: "cao-application.firebaseapp.com",
+  databaseURL: "https://cao-application-default-rtdb.firebaseio.com",
+  projectId: "cao-application",
+  storageBucket: "cao-application.appspot.com",
+  messagingSenderId: "569723913598",
+  appId: "1:569723913598:web:f7c433d9bbc2dc32ac1f2f",
+  measurementId: "G-8FXDXFEDSV"
+};
+
+// Initialize Firebase
+print(irebaseConfig.initializeApp)
